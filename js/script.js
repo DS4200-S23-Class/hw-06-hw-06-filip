ef7a6e38621ee982f5738225ba3abb7ed55f10b9
ftomovski <ftomovski@hotmail.com>
2023-03-02 17:18:59 -0500

    final

--- js/script.js
+++ js/script.js
@@ -1,160 +1,190 @@
+//Create constants for the frame
 const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500;
-const MARGINS = {left:50, right:50, top:50, bottom:50};
+const FRAME_WIDTH = 400; 
+const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
-const VIS_HEIGHT = FRAME_HEIGHT - (MARGINS.top + MARGINS.bottom);
-const VIS_WIDTH = FRAME_WIDTH - (MARGINS.left + MARGINS.right);
-
-//function to get border data
-function Border() {
-
-    console.log("1")
-    this.classList.toggle("add_border");
-    let circle_x = this.getAttribute("cx") / 50;
-    let circle_y = (500 - this.getAttribute("cy")) / 50;
-    let newText = "Last Point Clicked: (" + circle_x + ", " + circle_y + ")"
-    document.getElementById("print_points").innerHTML = newText;
-}
-//function to add border
-function addBorder() {
-    let circle_values = document.getElementsByTagName("circle");
-
-    for (let i = 0; i < circle_values.length; i++){
-        let circle_value = circle_values[i];
-        circle_value.addEventListener("click", Border);
-        console.log("2")
-    }
-}
-//function to get coordinates
-function getcoordinates() {
-
-    // Get X and Y coordinates
-    let x_ax = document.getElementById('X-ax');
-    let y_ax = document.getElementById('Y-ax');
-    let x = x_ax.value * 50;
-    let y = 500 - (y_ax.value * 50);
-
-    svg_plot = document.getElementById("svg_plot");
-    svg_plot.innerHTML += '<circle class="circle" cx=' + x + ' cy=' + y + ' r="10" onClick="addBorder()"/>';
-
-    }
-//function to send new point to svg
-function SendPoint() {
-    let new_point = document.getElementById("SendPoint");
-    new_point.addEventListener("click", getcoordinates);
-}
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
 
-const FRAME1 = d3.select("#vis1")
-    .append("svg")
-    .attr("height", FRAME_HEIGHT)
-    .attr("width", FRAME_WIDTH)
-    .attr("class", "frame");
 
-// Read CSV data
-d3.csv("data/scatter-data.csv").then((DATA) => {
+//Create Frames
+const FRAME1 = d3.select("#vis1") 
+                  .append("svg") 
+                    .attr("height", FRAME_HEIGHT)   
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame"); 
 
-    const X_SCALE = d3.scaleLinear()
-        .domain([0, 10])
-        .range([0, VIS_WIDTH]);
-    const Y_SCALE = d3.scaleLinear()
-        .domain([10, 0])
-        .range([0, VIS_HEIGHT]);
-    // Plot the points
-	FRAME1.selectAll("points")
-        .data(DATA)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left); })
-        .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.top) ; })
-        .attr("r", 6)
-        .attr("class", "point");
+const FRAME2 = d3.select("#vis2") 
+                    .append("svg") 
+                      .attr("height", FRAME_HEIGHT)   
+                      .attr("width", FRAME_WIDTH)
+                      .attr("class", "frame");
 
-	// Append Axis
-	FRAME1.append("g")
-		.attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
-		.call(d3.axisBottom(X_SCALE).ticks(10))
-        .attr("font-size", "15px");
-	FRAME1.append("g")
-		.attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
-		.call(d3.axisLeft(Y_SCALE).ticks(10))
-        .attr("font-size", "15px");
+const FRAME3 = d3.select("#vis3") 
+                    .append("svg") 
+                      .attr("height", FRAME_HEIGHT)   
+                      .attr("width", FRAME_WIDTH)
+                      .attr("class", "frame"); 
 
-    // Last clicked point
-    function pointClicked() {
+//Read CSV data
+d3.csv("data/iris.csv").then((data) => { 
 
-        let xCoord = d3.select(this).attr("cx");
-        let yCoord = d3.select(this).attr("cy");
+	//Petal Length vs Sepal Length scatter
+    const MAX_Y1 = d3.max(data, (d) => { return parseFloat(d.Petal_Length); });
+	const MAX_X1 = d3.max(data, (d) => { return parseFloat(d.Sepal_Length); });
+	
 
-        xCoord = Math.round(X_SCALE.invert(xCoord - MARGINS.left));
-        yCoord = Math.round(Y_SCALE.invert(yCoord - MARGINS.top));
-        
-        document.getElementById("last_point").innerHTML = "Last Point Clicked: (" + xCoord + "," + yCoord + ")";
+	//Create scales
+	const X_SCALE = d3.scaleLinear() 
+	                    .domain([0, (MAX_X1 + 2)]) 
+	                    .range([0, VIS_WIDTH]); 
+	const Y_SCALE = d3.scaleLinear() 
+	                    .domain([0, (MAX_Y1 + 2)]) 
+	                    .range([VIS_HEIGHT, 0]); 
 
-        this.classList.toggle('point-border');
-    }
+    const color = d3.scaleOrdinal()
+                        .domain(["setosa", "versicolor", "virginica"])
+                        .range(["red", "blue", "green"]);
 
-    function addPoint() {
-        let xCoord = document.getElementById("x-coord");
-        let yCoord =  document.getElementById("y-coord");
-
-        let x = xCoord.value;
-        let y = yCoord.value;
-
-
-        FRAME1.append("circle")
-            .attr("cx", (d) => { return (X_SCALE(x) + MARGINS.left); })
-            .attr("cy", (d) => { return (Y_SCALE(y) + MARGINS.top) ; })
-            .attr("r", 6)
+	   //Append svg
+        var point1 = FRAME1.append("g")
+        .   selectAll("point")  
+            .data(data) 
+            .enter()       
+            .append("circle")  
+            .attr("cx", (d) => { return (X_SCALE(d.Sepal_Length) + MARGINS.left); }) 
+            .attr("cy", (d) => { return (Y_SCALE(d.Petal_Length) + MARGINS.bottom); })
+            .attr("r", 5)
+            .style('opacity', 0.5)
+            .style("fill", (d) => {return color(d.Species);})
             .attr("class", "point")
-            .on("click", pointClicked);
-    }
-    d3.selectAll("#enter").on("click", addPoint);
-    d3.selectAll(".point").on("click", pointClicked);
-});
+           
+
+        //X axis
+    	FRAME1.append("g") 
+                .attr("transform", "translate(" + MARGINS.left + 
+                   "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+                .call(d3.axisBottom(X_SCALE)) 
+                .attr("font-size", '7px'); 
+
+        //Y axis
+        FRAME1.append("g") 
+            .attr("transform", "translate(" + (MARGINS.left) + 
+                 "," + (MARGINS.top) + ")") 
+            .call(d3.axisLeft(Y_SCALE)) 
+            .attr("font-size", '10px');
+    
+        //Petal Width vs Sepal Width scatter
+        const MAX_X2 = d3.max(data, (d) => { return parseFloat(d.Sepal_Width); });
+        const MAX_Y2 = d3.max(data, (d) => { return parseFloat(d.Petal_Width); });
+         
+        const X_SCALE2 = d3.scaleLinear() 
+                                 .domain([0, (MAX_X2 + 2)]) 
+                                 .range([0, VIS_WIDTH]); 
+        const Y_SCALE2 = d3.scaleLinear() 
+                                 .domain([0, (MAX_Y2 + 2)]) 
+                                 .range([VIS_HEIGHT, 0]);
+
+        
+    
+        var point2 = FRAME2.append("g")
+            .selectAll("circle")  
+            .data(data) 
+            .enter()       
+            .append("circle")  
+            .attr("cx", (d) => { return (X_SCALE2(d.Sepal_Width) + MARGINS.left); }) 
+            .attr("cy", (d) => { return (Y_SCALE2(d.Petal_Width) + MARGINS.bottom); })
+            .attr("r", 5)
+            .style('opacity', 0.5)
+            .style("fill", (d) => {return color(d.Species); })
+            .attr("class", "point")
+    
+        
+    //BRUSH constant
+    const BRUSH = d3.brush()
+                        .extent([ [0,0], [FRAME_WIDTH,FRAME_HEIGHT] ])
+                        .on("start brush", brushed);
+       
+
+    function brushed(event) {
+
+        
+        const selection = event.selection;
+
+        //Highlighting
+        point2.classed("selectedPoint", (d) => { return Been-Brushed(selection, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.bottom ); });
+        point1.classed("selectedPoint", (d) => { return Been-Brushed(selection, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.bottom ); });
+        bars.classed("selectedPoint", (d) => { return Been-Brushed(selection, X_SCALE2(d.Sepal_Width) + MARGINS.left, Y_SCALE2(d.Petal_Width) + MARGINS.bottom ); });
+            
+       
+       }; 
+    
+    //Finds points in frame
+    function Been-Brushed(brush_coords, cx, cy) {
+       var x0 = brush_coords[0][0],
+           x1 = brush_coords[1][0],
+           y0 = brush_coords[0][1],
+           y1 = brush_coords[1][1];
+      return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;  
+     };
+
+       
+  
+
+    FRAME2.call(BRUSH);
+
+	FRAME2.append("g") 
+            .attr("transform", "translate(" + MARGINS.left + 
+               "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+            .call(d3.axisBottom(X_SCALE)) 
+            .attr("font-size", '7px'); 
+
+     FRAME2.append("g") 
+        .attr("transform", "translate(" + (MARGINS.left) + 
+             "," + (MARGINS.top) + ")") 
+        .call(d3.axisLeft(Y_SCALE)) 
+        .attr("font-size", '10px');
+
+	const MAX_AMT = 50;
 
 
+    //Y axis scale
+	const AMT_SCALE = d3.scaleLinear() 
+	                    .domain([MAX_AMT + 10, 0]) 
+	                    .range([0, VIS_HEIGHT]); 
+
+	// create x axis scale based on category names
+    const CATEGORY_SCALE = d3.scaleBand() 
+                .domain(data.map((d) => { return d.Species; })) 
+                .range([0, VIS_WIDTH])
+                .padding(.2); 
 
 
-// Frame for visualization #2
-const FRAME2 = d3.select("#vis2")
-    .append("svg")
-    .attr("height", FRAME_HEIGHT)
-    .attr("width", FRAME_WIDTH)
-    .attr("class", "frame"); 
-
-// Read CSV data
-d3.csv("data/bar-data.csv").then((DATA) => {
- 
-	const MAX_Y = d3.max(DATA, (d) => { return parseInt(d.amount); });
-
-	const X_SCALE = d3.scaleBand()
-        .domain(DATA.map(function(d) { return d.category; }))
-        .range([0, VIS_WIDTH])
-        .padding(.2);
-	const Y_SCALE = d3.scaleLinear()
-        .domain([(MAX_Y+1), 0])
-        .range([0, VIS_HEIGHT]);
-
-    FRAME2.selectAll("bars")  
-        .data(DATA)
+    // plot bar based on data with rectangle svgs 
+	var bars = FRAME3.selectAll("bar")  
+        .data(data) 
         .enter()       
         .append("rect")  
-            .attr("x", (d) => { return X_SCALE(d.category) + MARGINS.left; }) 
-            .attr("y", (d) => { return Y_SCALE(d.amount) +  MARGINS.top; })
-            .attr("width", X_SCALE.bandwidth())
-            .attr("height", (d) => { return VIS_HEIGHT - Y_SCALE(d.amount); })
-            .attr("class", "bar") 
+          .attr("y", (d) => { return AMT_SCALE(MAX_AMT) + MARGINS.bottom; }) 
+          .attr("x", (d) => { return CATEGORY_SCALE(d.Species) + MARGINS.left;}) 
+          .attr("height", (d) => { return VIS_HEIGHT - AMT_SCALE(MAX_AMT); })
+          .attr("width", CATEGORY_SCALE.bandwidth())
+          .style("fill", (d) => {return color(d.Species); })
+          .attr("class", "bar");
 
-	// Append Axis
-	FRAME2.append("g")
-		.attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
-		.call(d3.axisBottom(X_SCALE).ticks(7))
-		.attr("font-size", '20px');  
-	FRAME2.append("g")
-		.attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
-		.call(d3.axisLeft(Y_SCALE).ticks(10))
-		.attr("font-size", '20px');
+	 FRAME3.append("g") 
+        .attr("transform", "translate(" + MARGINS.left + 
+              "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+        .call(d3.axisBottom(CATEGORY_SCALE))
+          .attr("font-size", '20px'); 
+
+	FRAME3.append("g") 
+	      .attr("transform", "translate(" + (MARGINS.left) + 
+	            "," + (MARGINS.top) + ")") 
+	      .call(d3.axisLeft(AMT_SCALE).ticks(10)) 
+	        .attr("font-size", '20px');
 
 
-});  
+        
+});
